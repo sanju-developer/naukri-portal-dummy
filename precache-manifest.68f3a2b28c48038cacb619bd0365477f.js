92c12dc6f5545497e673bf85c10f0c93a5fb56dd
sanju-developer <vivekrajoriya106@gmail.com>
2020-09-10 20:32:27 +0530

    Updates

---
+++ precache-manifest.68f3a2b28c48038cacb619bd0365477f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408d72bfc019df5843590a49c56921dd",
+    "url": "/sanju-developer/naukri-portal-dummy/index.html"
+  },
+  {
+    "revision": "6f8117a81f9cb6d018f2",
+    "url": "/sanju-developer/naukri-portal-dummy/static/css/main.022fdc10.chunk.css"
+  },
+  {
+    "revision": "dc1759e1b76f022f734f",
+    "url": "/sanju-developer/naukri-portal-dummy/static/js/2.a3c7e695.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sanju-developer/naukri-portal-dummy/static/js/2.a3c7e695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8117a81f9cb6d018f2",
+    "url": "/sanju-developer/naukri-portal-dummy/static/js/main.4135bb40.chunk.js"
+  },
+  {
+    "revision": "a8d62e600db95d526adc",
+    "url": "/sanju-developer/naukri-portal-dummy/static/js/runtime-main.0302ab59.js"
+  }
+]);
